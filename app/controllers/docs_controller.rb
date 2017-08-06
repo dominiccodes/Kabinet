233543d4a7975bb39dec551ccc59c149f069bb2a
dominiccodes <email@domcodes.com>
2017-08-06 17:44:11 +0100

    installed simple_form, set up form partial and defined new and create methods

--- app/controllers/docs_controller.rb
+++ app/controllers/docs_controller.rb
@@ -7,9 +7,17 @@ class DocsController < ApplicationController
 	end
 	
 	def new
+		@doc = Doc.new
 	end
 	
 	def create
+		@doc = Doc.new(doc_params)
+		
+		if @doc.save
+			redirect_to @doc
+		else
+			render 'new'
+		end
 	end
 	
 	def edit
@@ -23,9 +31,10 @@ class DocsController < ApplicationController
 	
 	private
 	
-	def find_doc
-	end
-	
-	def doc_params
-	end
+		def find_doc
+		end
+
+		def doc_params
+			params.require(:doc).permit(:title, :content)
+		end
 end
